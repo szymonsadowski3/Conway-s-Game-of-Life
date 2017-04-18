6871fcd68f396024e1b05f9e08f7907075d1a680
szymonsadowski3 <szymonsadowski3@gmail.com>
2017-04-18 13:59:04 +0200

    made cell distinction easy (grid visualization) and added some interesting patterns

--- source/game_of_life.py
+++ source/game_of_life.py
@@ -8,9 +8,10 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 DEFAULT_BG_COLOR = 'steel blue'
+DEFAULT_BG_COLOR2 = 'cornflower blue'
+DEFAULT_FOREGROUND_COLOR = 'black'
 
 class GameOfLifeGrid(Gtk.Window):
-    color_mapping = {0: DEFAULT_BG_COLOR, 1: 'black'}
 
     #HELPERS
     def determineWhichCellWasClicked(self, x, y):
@@ -33,13 +34,25 @@ class GameOfLifeGrid(Gtk.Window):
         cell = self.grid.get_child_at(column, row)
         GtkHelper.overrideCellColor(cell, color)
 
+    def changeCellColorToBG(self, row, column):
+        is_cell_odd = (row+column)%2==0
+        cell = self.grid.get_child_at(column, row)
+        if is_cell_odd:
+            GtkHelper.overrideCellColor(cell, DEFAULT_BG_COLOR)
+        else:
+            GtkHelper.overrideCellColor(cell, DEFAULT_BG_COLOR2)
+
+    def changeCellColorToForeground(self, row, column):
+        cell = self.grid.get_child_at(column, row)
+        GtkHelper.overrideCellColor(cell, DEFAULT_FOREGROUND_COLOR)
+
     def updateVisualization(self):
         for i in range(self.grid_height):
             for j in range(self.grid_width):
                 if self.model[i][j]:
-                    self.changeCellColor(i,j,'black')
+                    self.changeCellColor(i,j, DEFAULT_FOREGROUND_COLOR)
                 elif not self.model[i][j]:
-                    self.changeCellColor(i, j, DEFAULT_BG_COLOR)
+                    self.changeCellColorToBG(i, j)
 
     def nextStep(self, widget):
         self.game_of_life_simulator.simulate(self.model)
@@ -49,7 +62,10 @@ class GameOfLifeGrid(Gtk.Window):
     def toggleCellState(self, row, col):
         new_state = 1 - self.model[row][col]
         self.model[row][col] = new_state
-        self.changeCellColor(row, col, GameOfLifeGrid.color_mapping[new_state])
+        if new_state:
+            self.changeCellColorToForeground(row, col)
+        else:
+            self.changeCellColorToBG(row, col)
 
     #EVENTS
     def eventPress(self, eventbox, event):
